f8f644c960e61bb071058337156b8de6e6ca1797
TetianaOd <149709293+TetianaOd@users.noreply.github.com>
2023-11-20 15:56:54 +0200

    2 commit

--- var.js
+++ var.js
@@ -1,3 +1,7 @@
 var message;
-message = 'Hello, Siri'
-console.log(message)
+message = 'Hello, Siri';
+console.log(message);
+
+var newMessage;
+newMessage = 'Hello, Tanya';
+console.log(newMessage);
